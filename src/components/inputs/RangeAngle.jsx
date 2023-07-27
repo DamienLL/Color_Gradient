04eb5cc81b266097110eab0385871ff16dc46a4f
DamienLL <125370675+DamienLL@users.noreply.github.com>
2023-07-27 16:20:04 +0200

    resolution bug of RangeAngel.jsx

--- src/components/inputs/RangeAngle.jsx
+++ src/components/inputs/RangeAngle.jsx
@@ -1,14 +1,17 @@
-import { useDispatch } from "react-redux"
-import { updateColorValue } from "../../features/gradient"
+import { useDispatch, useSelector } from "react-redux"
+import {updateAngle} from "../../features/gradient"
 
-export default function ColorInput({id, color}) {
+export default function RangeAngle() {
   const dispatch = useDispatch()
+  const gradientValues = useSelector(state => state.gradient)
 
   return (
     <input 
-    onChange={e => dispatch(updateColorValue({id, value: e.target.value, type: "color"}))}
-    value={color}
-    className="w-[60px] h-[60px] bg-transparent rounded-md cursor-pointer d-block [&:not(:last-child)]:mr-4"
-    type="color" />
-  )
+    min="0"
+    max="360"
+    value={gradientValues.angle}
+    onChange={e => dispatch(updateAngle(e.target.value))}
+    className="w-full h-1 mb-10 bg-gray-200 rounded-lg cursor-pointer"
+    type="range" />
+    )
 }
